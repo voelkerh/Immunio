ff9cdc6d46f64b88f00a3847cfb03474a3f25e04
voelkerh <henriette.voelker@posteo.de>
2025-06-14 23:03:31 +0200

    Add implementation notes

--- src/react/Providers/PersonProvider.jsx
+++ src/react/Providers/PersonProvider.jsx
@@ -1,9 +1,12 @@
 import React, { createContext, useContext, useState } from 'react'
 
+// create new react context
 const PersonContext = createContext()
 
+// custom hook to use context in other components
 export const usePerson = () => useContext(PersonContext)
 
+// wrapper to provide context
 export const PersonProvider = ({ children }) => {
   const [person, setPerson] = useState({
     name: '',
