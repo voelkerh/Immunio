061eda427448f5bd4bf71c5c4d66598e93a36e09
voelkerh <henriette.voelker@posteo.de>
2025-06-15 17:13:31 +0200

    Load JSON data for reccomended travel vaccinations and show in list

--- src/react/Pages/Travel/CountryView.jsx
+++ src/react/Pages/Travel/CountryView.jsx
@@ -3,13 +3,19 @@ import { useParams } from 'react-router-dom'
 
 import { Stack } from '@mui/system'
 import { ArrowBack } from '@mui/icons-material'
-import { Typography } from '@mui/material'
+import { Typography, Divider, List, ListItem, ListItemText } from '@mui/material'
 
 import { useAppBar } from '../../Providers/AppBarProvider'
+
+import travelData from '../../../assets/travel_vaccinations.json'
 
 const CountryView = () => {
   const { name } = useParams()
   const { setConfig } = useAppBar()
+
+  const dividerSx = { width: '100%', my: 2 }
+  const listSx = { width: '100%' }
+  const listItemSx = { divider: true }
 
   useEffect(() => {
     setConfig({
@@ -20,17 +26,27 @@ const CountryView = () => {
     })
   }, [])
 
+  const recommendations = travelData[name.toLowerCase().replaceAll(' ', '-')]
+
   return (
     <Stack
       flex="1 1 auto"
       width="100%"
       height="100%"
-      justifyContent="center"
+      justifyContent="start"
       alignItems="center"
     >
       <Typography variant="h4">
         {name}
       </Typography>
+      <Divider sx={dividerSx}>Empfohlene Impfungen</Divider>
+      <List sx={listSx}>
+        {recommendations?.map((vaccine) => (
+          <ListItem key={vaccine} sx={listItemSx}>
+            <ListItemText primary={vaccine} />
+          </ListItem>
+        ))}
+      </List>
     </Stack>
   )
 }
