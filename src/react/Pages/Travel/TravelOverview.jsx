afd26756575380f396b0a412a18b9a75e9e23b60
voelkerh <henriette.voelker@posteo.de>
2025-06-15 16:05:46 +0200

    Add basic elements to travel overview page

--- src/react/Pages/Travel/TravelOverview.jsx
+++ src/react/Pages/Travel/TravelOverview.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { Stack, Button } from '@mui/material'
+import { Stack, Button, Divider, List, ListItem, ListItemText } from '@mui/material'
 import { Settings } from '@mui/icons-material'
 
 import { useAppBar } from '../../Providers/AppBarProvider'
+
+const generate = (element) => [0, 1, 2].map((value) => React.cloneElement(element, {
+  key: value
+}),)
 
 const TravelOverview = () => {
   const navigate = useNavigate()
 
   const { setConfig } = useAppBar()
+
+  const dividerSx = { width: '100%', my: 2 }
+  const listSx = { width: '100%' }
+  const listItemSx = { divider: true }
 
   useEffect(() => {
     setConfig({
@@ -31,6 +39,7 @@ const TravelOverview = () => {
         height="20%"
         rowGap="10px"
       >
+        <Divider sx={dividerSx}>Karte erkunden</Divider>
         <Button
           onClick={() => navigate('/travel/map')}
           style={{ border: 'none', background: 'none', padding: 0 }}
@@ -40,6 +49,26 @@ const TravelOverview = () => {
             alt="Map preview"
             style={{ width: '100%', height: 'auto', display: 'block' }}
           />
+        </Button>
+        <Divider sx={dividerSx}>Geplante Reisen</Divider>
+        <List sx={listSx}>
+          {generate(
+            <ListItem sx={listItemSx}>
+              <ListItemText
+                primary="Land"
+                secondary="Abfahrt"
+                onClick={() => {
+                  navigate('*')
+                }}
+              />
+            </ListItem>,
+          )}
+        </List>
+        <Button
+          variant="contained"
+          onClick={() => navigate('*')}
+        >
+          Reise hinzufügen
         </Button>
       </Stack>
     </Stack>
