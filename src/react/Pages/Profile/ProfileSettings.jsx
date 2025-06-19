67a8fed7320971cfdc1882ec91fe9eea86236000
David Kirchner <140542671+davidkirch@users.noreply.github.com>
2025-06-19 13:51:53 +0200

    fix: removed comment

--- src/react/Pages/Profile/ProfileSettings.jsx
+++ src/react/Pages/Profile/ProfileSettings.jsx
@@ -4,25 +4,19 @@ import { useNavigate } from 'react-router-dom'
 
 import { Typography, Stack } from '@mui/material'
 
-const ProfileSettings = () => {
-  const navigate = useNavigate()
-
-  return (
-    // This is a React Fragment. It is a virtual React Component that will not render a HTML-DOM-Element
-    // We need to use it here, because we want to render two Components (Typography, Button) with no parent around them.
-    <Stack
-      flex="1 1 auto"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
+const ProfileSettings = () => (
+  <Stack
+    flex="1 1 auto"
+    justifyContent="center"
+    alignItems="center"
+    width="100%"
+  >
+    <Typography
+      variant="h4"
     >
-      <Typography
-        variant="h4"
-      >
-        Here are your Settings
-      </Typography>
-    </Stack>
+      Here are your Settings
+    </Typography>
+  </Stack>
   )
-}
 
 export default ProfileSettings
