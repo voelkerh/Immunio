9c15a8e05610a4c390e33b1f260eb8253353b2a9
Micha <152519938+PizzaPichael@users.noreply.github.com>
2025-07-02 21:08:01 +0200

    feat: added risk group file to be used by settings and CreatePerson

---
+++ src/constants/riskGroups.js
@@ -0,0 +1,20 @@
+const riskGroups = [
+  'Bewohner*in In Altenpflege-/Pflegeeinrichtung',
+  'Beschäftigte*r In Altenpflege-/Pflegeeinrichtung',
+  'Älter Als 60',
+  'Enge Kontaktperson Von Menschen Mit Geschwächtem Immunsystem',
+  'Angeborene Oder Erworbene Abwehrschwäche (Immundefizienz)',
+  'Relevante Unterdrückung Des Abwehrsystems (Immunsuppression)',
+  'Chronische HerzKreislauferkrankungen',
+  'Chronische Erkrankungen Der Atmungsorgane',
+  'Chronische Lebererkrankungen',
+  'Chronische Nierenerkrankungen',
+  'Chronische Erkrankung Des Nervensystems',
+  'Demenz Oder Geistige Behinderung',
+  'Psychiatrische Erkrankungen',
+  'Stoffwechselerkrankungen, Wie Starkes Übergewicht Und Diabetes Mellitus (\'Zuckerkrankheit\')',
+  'Downsyndrom (Trisomie 21)',
+  'Aktive Krebserkrankungen.'
+]
+
+export default riskGroups
