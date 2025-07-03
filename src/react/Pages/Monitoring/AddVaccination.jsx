6527a7cda5f3d97a4b736d95da104c20cc129efe
voelkerh <henriette.voelker@posteo.de>
2025-07-03 16:56:16 +0200

    Add basic Structure for AddVaccination component

--- src/react/Pages/Monitoring/AddVaccination.jsx
+++ src/react/Pages/Monitoring/AddVaccination.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import { Button } from '@mui/material'
+import { Stack, Button } from '@mui/material'
 
 const AddVaccination = () => {
   const navigate = useNavigate()
   const { disease } = useParams()
   return (
-    <div>
-      {disease}
-      <Button onClick={() => navigate('/home')}>Speichern</Button>
-    </div>
+    <>
+      <Stack
+        flex="1 1 auto"
+        width="100%"
+        height="100%"
+        justifyContent="start"
+        alignItems="center"
+      >
+        {disease}
+      </Stack>
+      <Button variant="contained" onClick={() => navigate('/home')}>
+        Impfung hinzufügen
+      </Button>
+    </>
+
   )
 }
 
