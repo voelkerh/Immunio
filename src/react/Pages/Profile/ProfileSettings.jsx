b66768bb83bc036459eefb3124b1b1de326a56f9
Micha <152519938+PizzaPichael@users.noreply.github.com>
2025-07-03 14:06:43 +0200

    fix: removed debug statements

--- src/react/Pages/Profile/ProfileSettings.jsx
+++ src/react/Pages/Profile/ProfileSettings.jsx
@@ -22,7 +22,6 @@ const ProfileSettings = () => {
   const { person, setPerson } = usePerson()
 
   const weeks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-  console.log(person)
 
   return (
     <Stack
