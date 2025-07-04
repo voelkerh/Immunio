3c27b3d0ad372831d4caac7205f0fe3e084e1140
voelkerh <henriette.voelker@posteo.de>
2025-07-04 14:43:36 +0200

    Update imports and add basic logic for open vaccination list

--- src/react/Pages/Monitoring/OpenTravelVaccList.jsx
+++ src/react/Pages/Monitoring/OpenTravelVaccList.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
 import { List, ListItem, ListItemText } from '@mui/material'
-import { usePerson } from './PersonProvider'
-import getCountryRecommendations from '../Utils/getCountryRecommendations'
-import getMissingTravelVaccinationsForCountry from '../Utils/getMissingTravelVaccinationsForCountry'
+import { usePerson } from '../../Providers/PersonProvider'
+import getCountryRecommendations from '../../Utils/getCountryRecommendations'
+import getMissingTravelVaccinations from '../../Utils/getMissingTravelVaccinations'
 
 const listSx = { width: '100%' }
 const listItemSx = { divider: true }
@@ -13,25 +13,27 @@ const OpenTravelVaccList = () => {
   const { person } = usePerson()
   const { plannedTrips } = person
 
+  const getListElements = () => {
+    const travelDestinations = person?.plannedTrips?.map(trip => trip.country)
+    const allRecommendations = [
+      ...new Set(
+        travelDestinations.flatMap(country => getCountryRecommendations(country))
+      )
+    ]
+    return getMissingTravelVaccinations(allRecommendations, person?.vaccinations || [])
+  }
+
+  const listElements = getListElements()
+
   return (
     <List sx={listSx}>
-      {plannedTrips.map((trip) => {
-      const recommendations = getCountryRecommendations(trip.country)
-      (
-        <ListItem sx={listItemSx}>
-          <ListItemText
-            primary="Vaccination"
-            secondary={trip.country}
-            onClick={() => {
-              navigate('*')
-            }}
-          />
+      {listElements.map((vaccination) => (
+        <ListItem key={vaccination} sx={listItemSx}>
+          <ListItemText primary={vaccination} />
         </ListItem>
-      )
-      )}
-    }
+      ))}
     </List>
-)
+  )
 }
 
 export default OpenTravelVaccList
